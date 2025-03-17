1df36b7b051ed81e57616b9c9ca0b293021c05db
Sourav Das <124785348+Sourav07-D@users.noreply.github.com>
2025-03-17 23:27:58 +0530

    Add files via upload

---
+++ PropIntel - AI-Powered Real Estate Analytics & Price Prediction/Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to PropIntel! ")
+st.write("### An AI-Powered Real Estate Analytics & Price Prediction WebApp! 👋")
+
+st.sidebar.success("Select a demo above.")
